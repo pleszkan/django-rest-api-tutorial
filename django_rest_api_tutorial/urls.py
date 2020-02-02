3ec68da89b8b6565947f5c44a43bf210f89a9917
pleszkan <60507985+pleszkan@users.noreply.github.com>
2020-02-02 17:15:22 +0100

    Chapter 2

--- django_rest_api_tutorial/urls.py
+++ django_rest_api_tutorial/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from .snippets import views
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
